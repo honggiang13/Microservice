18d7863e52dafb37b59599bd7cd99e8fcd6857a3
honggiang13 <honggiang13@gmail.com>
2018-05-11 01:18:58 +0700

    Create projects partern

---
+++ Microservices.Service.Client/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Service.Client
+{
+    public class Class1
+    {
+    }
+}
