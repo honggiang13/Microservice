68ab058d96e9ec3b62f05e97c941dc75dd1b4702
Nguyen Hong Giang <honggiang13@gmail.com>
2018-05-11 17:26:42 +0700

    Add middleware class

---
+++ Microservices.Service.ConsoleHost/Middleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Microservices.Service.ConsoleHost
+{
+    class Middleware : OwinMiddleware
+    {
+        public Middleware(OwinMiddleware next)
+            :base(next)
+        {
+
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            context.Response.Headers["MachineName"] = "demo machine name";
+            await Next.Invoke(context);
+        }
+    }
+}
--- Microservices.Service.ConsoleHost/Startup.cs
+++ Microservices.Service.ConsoleHost/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Owin;
+﻿using System;
+using Owin;
 
 namespace Microservices.Service.ConsoleHost
 {
@@ -6,11 +7,28 @@ namespace Microservices.Service.ConsoleHost
     {
         public void Configuration(IAppBuilder app)
         {
+            //app.Use(async (context, next) =>
+            //{
+            //    context.Response.Headers["Product"] = "Web Api and Owin Middleware";
+
+            //    await next.Invoke();
+            //});
+
+            //app.Use(typeof(Middleware));
+
+            //var config = ConfigureWebApi();
+
+
             app.Run(context =>
             {
                 context.Response.ContentType = "text/plain";
                 return context.Response.WriteAsync("Hello, world.");
             });
         }
+
+        //private HttpConfiguration ConfigureWebApi()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
